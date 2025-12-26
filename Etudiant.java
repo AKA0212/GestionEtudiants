f92f9c45a3ad3f77e0afaccf8fe3f66b1d618533
amsKane <kaneams17@gmail.com>
2025-12-26 11:39:06 +0000

    Declaration des constructeurs avec parametre et sans parametre

--- Etudiant.java
+++ Etudiant.java
@@ -7,8 +7,20 @@ class Etudiant{
     private String nom;
     private String email;
 
-    
 
-    
+    //Constructeur sans parametre(valeur par defaut 0 pour les nombres et null pour les string)
+
+    Etudiant(){
+
+    }
+
+    //Constructeur avec parametre nous permet de passer des valeurs lors de l'instanciation
+    Etudiant(int numero, String prenom, String nom, String email){
+        this.numero = numero;
+        this.prenom = prenom;
+        this.nom = nom;
+        this.email = email;
+    }
+
 }
 
